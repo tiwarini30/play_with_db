105589f1584a84420ce967b791764d85cbffea76
Nitish Tiwari <163543317+tiwarini30@users.noreply.github.com>
2026-07-29 20:21:26 +0530

    Add files via upload

---
+++ JDBC_DEMO/src/main/java/JDBCDemo.java
@@ -0,0 +1,90 @@
+import java.sql.*;
+
+public class JDBCDemo {
+
+    private static  final String URL="jdbc:mysql://localhost:3306/jdbcdemo_db";
+    private static  final String USER="root";
+    private static  final String PASSWORD="Tiwari@123";
+
+    public static void main(String[] args) {
+//        Connection conn = null;
+//        try{
+//            conn = DriverManager.getConnection(URL,USER,PASSWORD);
+//            System.out.println("Connected to the database");
+//        }catch (SQLException e){
+//            e.printStackTrace();
+//        }finally {
+//            try {
+//                conn.close();
+//                System.out.println("Connection close");
+//            } catch (SQLException e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+        // conection with try with resource without close statment
+
+        try(Connection conn = DriverManager.getConnection(URL,USER,PASSWORD);){
+            System.out.println("Connected to the database");
+            //insertStudent(conn, "alice", "a;lice@gmail.com");
+            //insertStudent(conn, "tiwari", "tiwari@gmail.com");
+            updateStudent(conn, 2,"bob","bob@gmail.com");
+
+            selectStudent(conn);
+           // deleteStudent(conn, 1);
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void insertStudent(Connection conn, String name, String email){
+        String sql = "INSERT INTO students (name,  email) values('" +name+ "','" +email + "')";
+        try(Statement stmt=conn.createStatement()){
+            int rows=stmt.executeUpdate(sql);  //this is return int type
+            System.out.println("Insert: "+rows);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    // select statment
+    public static void selectStudent(Connection conn){
+        String sql = "SELECT * FROM students";
+        try(Statement stmt= conn.createStatement()){
+            ResultSet resultSet = stmt.executeQuery(sql);
+            System.out.println("Students list : ");
+            while(resultSet.next()){
+                int id=resultSet.getInt("id");
+                String name=resultSet.getString("name");
+                String email=resultSet.getString("email");
+                System.out.println(id + " : "+ name + " : "+ email);
+            }
+
+        } catch (SQLException e) {
+             e.printStackTrace();
+        }
+    }
+
+    private static void updateStudent( Connection conn , int id , String name,String email){
+        //String sql = " UPDATE students set name ='" + name + "', email = '"+email + "' where id =" +id ;
+        // update students set name = 'alice', email='email.com'
+        String sql = " UPDATE students set name = ?, email =  ? where id =  ?" ;
+        try(PreparedStatement pstmt =conn.prepareStatement(sql)){
+            pstmt.setString(1,name);
+            pstmt.setString(2,email);
+            pstmt.setInt(3,id);
+            int row = pstmt.executeUpdate();
+            System.out.println("upated : "+ row);
+        } catch (SQLException e) {
+             e.printStackTrace();
+        }
+    }
+    private static void deleteStudent(Connection conn , int id ){
+        String sql = " Delete from students where id ="+id;
+        try(Statement stmt =conn.createStatement()){
+            int row = stmt.executeUpdate(sql);
+            System.out.println("Deleted : "+ row);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
---
+++ JDBC_DEMO/src/main/java/TransactionDemo.java
@@ -0,0 +1,77 @@
+import java.sql.*;
+
+public class TransactionDemo {
+
+    private static  final String URL="jdbc:mysql://localhost:3306/jdbcdemo_db";
+    private static  final String USER="root";
+    private static  final String PASSWORD="Tiwari@123";
+// database connection with try resource
+    public static void main(String[] args) {
+        try(Connection conn = DriverManager.getConnection(URL,USER,PASSWORD)){
+            System.out.println("Connected to the database");
+            // transcation process begin---
+            // turned off auto commit that means NO auto save
+            conn.setAutoCommit(false);
+            try{
+                //two table add operation on this orders , orderItems
+                // insert into orders
+                int orderId= insertOrder(conn,101,"alice01",2000.0);
+                //insert into order_items
+                insertOrderItems(conn, orderId,"laptop",1,2000.0);
+                // manuually commit
+                conn.commit();
+                System.out.println("transcation committed successfully --- ");
+            } catch (Exception e) {
+             e.printStackTrace();
+             conn.rollback();
+                System.out.println("operation roll back ");
+        } finally {
+                conn.setAutoCommit(true);
+            }
+        } catch (SQLException e) {
+             e.printStackTrace();
+        }
+    }
+
+    private static int insertOrder(Connection conn, int customerId, String customerName, double price) {
+        String sql = "INSERT INTO orders(user_id, customer_name,total_amount) VALUES (?,?,?)";
+        try(PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){  // auto_increament
+            pstmt.setInt(1,customerId);
+            pstmt.setString(2,customerName);
+            pstmt.setDouble(3,price);
+            int rows = pstmt.executeUpdate();
+            System.out.println("INSERTED into orders: " + rows);
+            // generate orderID
+            try(ResultSet rs = pstmt.getGeneratedKeys()){
+                if(rs.next()){
+                    int orderId=rs.getInt(1);
+                    System.out.println("ORDERED ID: "+ orderId);
+                    return orderId;
+                }else{
+                    throw new SQLException("ORDER ID not generated... ! ");
+                }
+            }
+        } catch (SQLException e) {
+             throw new RuntimeException();
+        }
+    }
+
+    private static void insertOrderItems(Connection conn, int orderId, String productName, int quantity, double price) {
+        String sql = "INSERT INTO order_items(order_id, product_name,quantity, price) VALUES (?,?,?,?)";
+        try(PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){  // auto_increament
+            pstmt.setInt(1,orderId);
+            pstmt.setString(2,productName);
+            pstmt.setInt(3,quantity);
+            pstmt.setDouble(4,price);
+
+            //int x=10/0; // if we throw exception
+            int rows = pstmt.executeUpdate();
+            System.out.println("INSERTED into order_items: " + rows);
+            // generate orderID
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
